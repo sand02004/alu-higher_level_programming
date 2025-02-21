fdfe37b0b5e02d08c04a902aee7a240c207cf5af
sando2004 <s.mugwaneza@alustudent.com>
2025-02-21 12:47:54 -0800

    fdr

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
+def no_c(my_string):
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
