f7f6347da2049de0e051a24a286d1204bdfaa70e
sando2004 <s.mugwaneza@alustudent.com>
2025-03-05 04:16:25 -0800

    tt

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 6-rectangle.py
+# 7-rectangle.py
 """Defines a Rectangle class."""
 
 
@@ -7,9 +7,11 @@ class Rectangle:
     """Represent a rectangle.
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
     """
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -66,7 +68,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
