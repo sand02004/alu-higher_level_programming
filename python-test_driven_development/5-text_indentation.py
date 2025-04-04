9a81a6cc8d0477fe0a27d3c356b273213f50db27
sando2004 <s.mugwaneza@alustudent.com>
2025-04-04 13:37:22 -0700

    r

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,80 +1,27 @@
-::
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
 
-    >>> print_square = __import__('4-print_square').print_square
-    >>> print_square(1)
-    #
 
-::
+def text_indentation(text):
+    """A function that prints two new lines after the char ...
+        :param text:
+        :type text:; string
+        :raise TypeError: if text is not a string
+        """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-    >>> print_square(4)
-    ####
-    ####
-    ####
-    ####
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-::
-
-    >>> print_square(10)
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-    ##########
-
-If ``size`` is zero, the function prints nothing.
-
-::
-
-    >>> print_square(0)
-
-Invalid Sizes
-=============
-
-The parameter ``size`` must be an integer. Otherwise, a TypeError is raised.
-
-::
-
-    >>> print_square("not an int")
-    Traceback (most recent call last):
-    TypeError: size must be an integer
-
-::
-
-    >>> print_square(5.5)
-    Traceback (most recent call last):
-    TypeError: size must be an integer
-
-::
-
-    >>> print_square(None)
-    Traceback (most recent call last):
-    TypeError: size must be an integer
-
-If ``size`` is less than zero, a ValueError is raised.
-
-::
-
-    >>> print_square(-7)
-    Traceback (most recent call last):
-    ValueError: size must be >= 0
-
-Note that type-checking occurs before value-checking.
-
-::
-# Temporary change to force git to see modification
-    >>> print_square(-7.5)
-    Traceback (most recent call last):
-    TypeError: size must be an integer
-
-At least one argument must be provided.
-
-::
-
-    >>> print_square()
-    Traceback (most recent call last):
-    TypeError: print_square() missing 1 required positional argument: 'size'
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
