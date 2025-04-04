638b5b16f974a3d8f5443100d1ece80d28e349ba
sando2004 <s.mugwaneza@alustudent.com>
2025-04-04 12:48:48 -0700

    D

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,56 +1,20 @@
-::
+#!/usr/bin/python3
+"""Defines a name-printing function."""
 
-    >>> say_my_name = __import__('3-say_my_name').say_my_name
-    >>> say_my_name("Beta", "Scribbles")
-    My name is Beta Scribbles
 
-::
+def say_my_name(first_name, last_name=""):
+    """Print the inputed name
 
-    >>> say_my_name("Tom Jerry", "Avril")
-    My name is Tom Jerry Avril
-
-The parameter ```last_name``` is optional. If no last name is provided,
-an empty string is printed instead.
-
-::
-
-    >>> say_my_name("Beta")
-    My name is Beta 
-
-Invalid Names
-=============
-
-The parameters ``first_name`` and ``last_name``` must be strings. Otherwise,
-a TypeError is raised.
-
-::
-
-    >>> say_my_name(6, "James")
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-::
-
-    >>> say_my_name("MeCalister", ["Cavs", "Bakers", "Heat"])
-    Traceback (most recent call last):
-    TypeError: last_name must be a string
-
-::
-
-    >>> say_my_name({"LeBron": 6, "Lames": 23}, 7.7)
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-::
-
-    >>> say_my_name(None)
-    Traceback (most recent call last):
-    TypeError: first_name must be a string
-
-At least one name must be provided.
-
-::
-
-    >>> say_my_name()
-    Traceback (most recent call last):
-    TypeError: say_my_name() missing 1 required positional argument: 'first_name'
+    :param: first_name
+    :type first_name: string
+    :raise: TypeError if first_name not string
+    :param: last_name
+    :type last_name: string
+    :raises: TypeError if last_name not string
+    :returns: none
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
