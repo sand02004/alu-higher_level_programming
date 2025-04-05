613dfad20b2b214a9d1bed4da9c539a25f0ad02d
sando2004 <s.mugwaneza@alustudent.com>
2025-04-05 13:59:56 -0700

    Sc

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and
-displays all values in the states"""
-#Write a script that takes in an argument and displays all values in the states
+"""Script that takes in an argument and displays all values in the states."""
 
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
+    # Connecting to the database
     conn = MySQLdb.connect(
         user=sys.argv[1],
         password=sys.argv[2],
@@ -16,13 +14,15 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = conn.cursor()
-    sql = """ SELECT * FROM states
-        WHERE name LIKE BINARY '{}'
-        ORDER BY id ASC """.format(sys.argv[4])
+    
+    # Using a parameterized query to avoid SQL injection
+    sql = """SELECT * FROM states
+             WHERE name LIKE BINARY %s
+             ORDER BY id ASC"""
+    cursor.execute(sql, (sys.argv[4],))
 
-    cursor.execute(sql)
+    # Fetching and printing data
     data = cursor.fetchall()
-
     for row in data:
         print(row)
 
